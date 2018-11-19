6e253fc20086ece7c326796618c8d488c3cc02ed
Patrick Woodburne <p.wood@live.ca>
2018-11-19 13:42:33 -0500

    Update venueList.js

--- src/venueList.js
+++ src/venueList.js
@@ -5,7 +5,7 @@ export default class VenueList extends Component{
   render(){
     /*Inside the sidebar and below the search bar, display the following list of venues via the listItem component*/
     return(
-        <ul tabIndex="3" className="venueList">
+        <ul tabIndex="3" aria-labelledby="Venue Names" className="venueList">
           {this.props.venues && this.props.venues.map((venue,idx) => (
           <ListItem key={idx} {...venue} handleListItemClick={this.props.handleListItemClick}/>
         ))}
@@ -13,3 +13,4 @@ export default class VenueList extends Component{
     );
   }
 }
+
