913b235d7eb9de1ce2e7fa0cf0d89126d347ddf4
Patrick Woodburne <p.wood@live.ca>
2018-11-19 17:39:30 -0500

    refactor: add button role to list items for accessibility

--- src/listItem.js
+++ src/listItem.js
@@ -4,7 +4,7 @@ export default class ListItem extends Component{
   render(){
     /*For each venue loaded from the foursquare api, provide a clickable list item of the venue's name*/
     return(
-      <li tabIndex="3" className="listItem"
+      <li tabIndex="3" role="button" className="listItem"
           onClick={() => this.props.handleListItemClick(this.props)}>
         {this.props.name}
       </li>
